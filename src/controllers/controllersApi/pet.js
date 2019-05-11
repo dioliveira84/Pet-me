6a81aa52e066ae0d6c0be9f290f72b37e1b464d9
Gabriel Sicuto <gabrielsicuto@gmail.com>
2019-05-11 13:52:02 -0300

    ajustes - foi alterado o nome do bucket de upload-petme para uploads-petme, com essa alteracao conseguimos salvar uma imagem no bucket AWS e no mongo.

--- src/controllers/controllersApi/pet.js
+++ src/controllers/controllersApi/pet.js
@@ -1,32 +1,32 @@
 const pet = require('../../models/v1/pet');
 
-module.exports.createPet =  async (req, res, next) => {
-   const { originalname: name, size, key, location: url = "" } = req.file;
+module.exports.createPet = async (req, res) => {
+  const {
+    originalname: name, size, key, location: url = '',
+  } = req.file;
 
-   const post = await pet.create({
-      name,
-      size,
-      key,
-      url
-    });
-  
-    return res.json(post);
+  const post = await pet.create({
+    name,
+    size,
+    key,
+    url,
+  });
 
-}
+  res.json(post);
+};
 
- module.exports.listPet =  (req, res, next) => {
-    res.send("Listar Pets");
- }
- module.exports.updatetPet =  (req, res, next) => {
-    res.send("Atualizar Pet");
- }
+module.exports.listPet = (req, res, next) => {
+  res.send('Listar Pets');
+};
+module.exports.updatetPet = (req, res, next) => {
+  res.send('Atualizar Pet');
+};
 
- module.exports.deletetPet =  async (req, res, next) => {
+module.exports.deletetPet = async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
 
-   const post = await Post.findById(req.params.id);
+  await post.remove();
 
-   await post.remove();
- 
-   return res.send();
-    res.send("Deletar Pet");
- }
+  return res.send();
+  res.send('Deletar Pet');
+};
