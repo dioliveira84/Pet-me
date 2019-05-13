89859c1d22c3274c5cfc5232e2addbba696741d9
Wilkor <34819624+Wilkor@users.noreply.github.com>
2019-05-13 15:28:26 -0300

    upadate
    removendo chave do sendgrid

--- src/config/configEmail.js
+++ src/config/configEmail.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-     sendgrigKey:'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',
+     sendgrigKey:'',
      msgWelcome:' Olá, <strong> {0}</strong> seja bem vindo ao Pet-me, a plataforma que irá conectar você ao seu futuro Pet!',
 
 }
