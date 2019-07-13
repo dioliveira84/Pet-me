e9ec903497c9229ffcb98b02e68f9ed0dcd3e7b6
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-07-13 19:47:50 -0300

    ajuste no login axios

--- src/controllers/controllersApi/login.js
+++ src/controllers/controllersApi/login.js
@@ -3,9 +3,11 @@ const passport = require('passport');
 
 module.exports.userLogin = (req, res,next) => {
 
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  })(req, res, next);
+  //passport.authenticate('local', {
+   // successRedirect: '/',
+    //failureRedirect: '/login',
+    //failureFlash: true
+  //})(req, res, next);
+
+  res.status(200).json({user:user})
 };
