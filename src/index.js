b100293f6bae519f257017daa1e54b07a5c3c467
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-07-20 17:31:26 -0300

    leberando cors

--- src/index.js
+++ src/index.js
@@ -24,7 +24,12 @@ require('dotenv').config({
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan());
