db7def85c9fb0bbc1bcc14873cb6aa186dfbe773
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-07-23 22:16:25 -0300

    criando list de anexo mediante a id do projeto

--- src/routes/api/v1/post.js
+++ src/routes/api/v1/post.js
@@ -12,7 +12,7 @@ const authServiceJwt = require('../../../config/authJwt')
 
 
 router.route('/file').post(multer(multerConfig).single('file'), controller.createFile);
-router.route('/list').get(controller.listAnexo);
+router.route('/list/:id').get(controller.listAnexo);
 router.route('/delete/:id').delete(controller.deleteAnexo)
 
 module.exports = router;
