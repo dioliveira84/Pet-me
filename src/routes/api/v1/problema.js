43941b29168a5e06f92e995a9b1d2b7e471dc733
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-07-23 23:28:50 -0300

    adicionando dados para cadastrar a inciativa

--- src/routes/api/v1/problema.js
+++ src/routes/api/v1/problema.js
@@ -7,10 +7,13 @@ const router = express.Router();
 const multer = require('multer');
 //const multerConfig = require('../../../config/multer')
 //multer(multerConfig).single('file')
+//const configAuth = require('../../../config/auth');
 
-const configAuth = require('../../../config/auth');
+const authServiceJwt = require('../../../config/authJwt')
 
-router.route('/create').post(configAuth.ensureAuthenticated, controller.createProblem);
+
+
+router.route('/create').post(authServiceJwt.authorize,controller.createProblem);
 router.route('/list').get(controller.listProblem);
 //router.route('/update').post(configAuth.ensureAuthenticated,controller.updatetPet);
 //router.route('/delete/:id').post(configAuth.ensureAuthenticated,controller.deletetPet);
